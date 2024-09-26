39941dd4a0c9d06f97aede403152d90914def561
Kniev <agnivag.16@gmail.com>
2024-09-26 19:34:11 -0400

    added default values

---
+++ ActiProject - the hopping game/Main.java
@@ -0,0 +1,12 @@
+import ActiverseEngine.*;
+
+/**
+ * Instantiates a new myWorld class and starts the game engine
+ *
+ * @author Knivier
+ */
+public class Main {
+    public static void main(String[] args) {
+        Activerse.start(new Level(400,400,1));
+   }
+}
